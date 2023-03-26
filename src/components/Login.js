3da027aa8d8ba4c046f92ecd82b79301ea5e6e87
Gonzalo Almiron <gonzaloalmiron.25@gmail.com>
2023-03-26 00:54:44 -0300

    Auth google

--- src/components/Login.js
+++ src/components/Login.js
@@ -61,8 +61,8 @@ const Login = (props) =>  {
               <p>No tenes usuario?</p>
               <Link to="/signup">Registrate aquí</Link>
             <hr/>
-              <Link to="http://localhost:3002/api/auth/google"> Logear con google </Link>
-            
+              <Link to="http://localhost:3002/google"> Logear con google </Link>
+
             </div>
         </div>
       </form>
